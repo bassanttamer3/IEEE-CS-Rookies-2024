cf391205b4e404f3cfd3f6bdbddc96997c171f92
bassanttamer3 <115741505+bassanttamer3@users.noreply.github.com>
2023-12-26 01:39:15 +0200

    Update EncryptAndDecryptMessage.java

--- Task_2/EncryptAndDecryptMessage.java
+++ Task_2/EncryptAndDecryptMessage.java
@@ -34,40 +34,4 @@ public class EncryptAndDecryptMessage {
         }
     }
 }
-=======
 
-package encrypt.and.decrypt.message;
-
-import java.util.Scanner;
-
-public class EncryptAndDecryptMessage {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int Q = scanner.nextInt();
-        scanner.nextLine();
-        String S = scanner.nextLine();
-        String key = "PgEfTYaWGHjDAmxQqFLRpCJBownyUKZXkbvzIdshurMilNSVOtec#@_!=.+-*/";
-        String original = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        int n = S.length();
-
-        if (Q == 1) {
-            for (int i = 0; i < n; i++) {
-                char currentChar = S.charAt(i);
-                int index = original.indexOf(currentChar);
-                if (index != -1) {
-                    System.out.print(key.charAt(index));
-                }
-            }
-        } else {
-            for (int i = 0; i < n; i++) {
-                char currentChar = S.charAt(i);
-                int index = key.indexOf(currentChar);
-                if (index != -1) {
-                    System.out.print(original.charAt(index));
-                }
-            }
-        }
-    }
-}
->>>>>>> 446e33e316cf490399143f7b92cc8e58b8a16e02
