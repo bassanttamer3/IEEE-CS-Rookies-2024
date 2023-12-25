140210f07abd5c8f2a7c5c90c106d0f2109c3e71
bassanttamer3 <115741505+bassanttamer3@users.noreply.github.com>
2023-12-26 01:38:37 +0200

    Update CreateANewString.java

--- Task_2/CreateANewString.java
+++ Task_2/CreateANewString.java
@@ -19,27 +19,3 @@ public class CreateANewString {
 }
     
     
-
-=======
-
-package create.a.pkgnew.string;
-
-import java.util.Scanner;
-
-
-public class CreateANewString {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String S = scanner.nextLine().trim();
-        String T = scanner.nextLine().trim();
-        System.out.println(S.length() + " " + T.length());
-  
-        System.out.println(S + " " + T);
-    scanner.close();
-    }
-}
-    
-    
-
->>>>>>> 446e33e316cf490399143f7b92cc8e58b8a16e02
